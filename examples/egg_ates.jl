98efb2ff2d504f6ca1754530f13c08bc9a3d4cfc
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2025-02-26 23:43:09 +0100

    Add EGG ATES example

---
+++ examples/egg_ates.jl
@@ -0,0 +1,186 @@
+# # Digital twinning of a high-temperature aquifer thermal energy storage system
+#
+# This script was prepared for the 2025 DTE & AICOMAS conference
+#
+# The example demonstrates digital twinning of high-temperature aquifer thermal
+# energy storage (HT-ATES). We first set up and simulate  high-fidelity model of
+# the system, before we construct reduced-order models at several resolutions
+# and calibrate using adjoint-based optimization so that their output matches
+# that of the high-fidelity model.
+
+# Add modules to namespace
+using Jutul, JutulDarcy # Jutul and JutulDarcy modules
+using Fimbul # GHOTSTDigiT likely to be removed
+using HYPRE # Iterative linear solvers
+using GLMakie # Visualization
+
+## # Set up model
+# We use the first realization of the EGG benchmark model [cite], and place a
+# well doublet near the center of the domain. The fluid model is a single-phase
+# water system with PVT formulations taken from the NIST database [cite], which
+# can be conveniently set up with the :geothermal keyword.
+#
+# The HT-ATES system is operated by charging the aquifer through the main well
+# (labelled "Hot" in this setup) with water at 25 l/s and 90°C from June to
+# September while a supporting well (labelled "Cold") is used to extract water
+# at a constant BHP of 25 bar. The system is then discharged from December to
+# March by producing hot water from the main well at a rate of 25 l/s with the
+# supporting well injecting water at 10°C at a BHP of 45 bar. For the remaining
+# monthsm the system is left to rest with no external forces applied. This cycle
+# of charge -- rest -- discharge -- rest is repeated for a total of 5 years.
+hifi = egg_ates(;
+    use_bc = false, report_interval = si_unit(:year)/12/4)
+
+## ## Visualize the model
+# We visualize the model interactively using `plot_reservoir`.
+plot_reservoir(hifi, reservoir_model(hifi.model).data_domain)
+
+## ## Simulate high-fidelity model
+# We set up a simulator for the high-fidelity model and simulate the system.
+results_hifi = simulate_reservoir(hifi)
+
+## ### Visualize the reservoir states
+# We visualize the results of the high-fidelity simulation interactively using
+# `plot_reservoir`. We see that a hot thermal plume develops around the main
+# well, while a cold plume develops around the supporting well. After a few
+# cycles, the plumes start to interact slightly.
+plot_reservoir(hifi, results_hifi.states, step = length(hifi.dt))
+
+## ### Inspect well output
+# We can also inspect the well output using `plot_well_results`.
+plot_well_results(results_hifi.wells)
+
+## ## Construct proxy model
+# The high-fidelity model is posed on a logicaly Cartesian mesh with 60×60×7
+# cells. We construct a proxy models by coarsening the high-fidelity model to
+# 15×15×1 cells using the `coarsen_reservoir_case` function.
+coarsening = (15,15,3)
+proxy = JutulDarcy.coarsen_reservoir_case(hifi, coarsening,
+    method=:ijk,
+    setup_arg = (block_backend = true,);
+)
+results_proxy = simulate_reservoir(proxy, info_level=0)
+plot_reservoir(proxy, results_proxy.states, step = length(proxy.dt))
+
+## ### Compare proxy models to high-fidelity model
+# We compare the well output of the proxy models to the high-fidelity model.
+plot_well_results([results_hifi.wells, results_proxy.wells],
+    names = ["High-fidelity", "Proxy"])
+
+
+# ### Define mismatch objective function
+# We define an objective function that measures the mismatch in well output
+# between the high-fidelity model and the proxy model. The objective function is
+# defined as the sum of the squared differences in production temperature of all
+# wells, weighted by the inverse of the units of the respective quantities. The
+# objective function is also scaled by the total time simulated in the
+# high-fidelity model.
+states_hf = results_hifi.result.states
+states_proxy = results_proxy.result.states
+# We calibrate against the first two years. Since we have defined report steps
+# of 1/4 month, this corresponts to the first 12*4*2 steps. To see the effect of
+# more or less data used for calibration, and to exclude data from one or more
+# of the wells and WellObs, you can change num_years_cal and wells_cal below.
+num_years_cal = 2
+n_steps = 12*4*num_years_cal
+wells_cal = [:WellA, :WellObs, :WellB]
+objective = (model, state, dt, step_no, forces) ->
+    well_mismatch_thermal(model, wells_cal,
+        states_hf, state, dt, step_no, forces;
+        scale=sum(hifi.dt[1:n_steps]),
+        w_bhp = 0.0,
+        w_temp = 1.0/si_unit(:Kelvin), 
+        w_energy = 0.0,
+    )
+
+# ### Compute mismatch for initial proxy model
+obj0 = Jutul.evaluate_objective(
+    objective, proxy.model, states_proxy[1:n_steps], 
+    proxy.dt[1:n_steps], proxy.forces[1:n_steps])
+println("Initial proxy mismatch: $obj0")
+
+## ## Set up optimization
+# We start by declaring the parameters to be optimized and their bounds
+parameters = setup_parameters(proxy.model)
+opt_config = optimization_config(proxy_cal.model, parameters,
+    use_scaling = true,
+    rel_min = 1e-3,
+    rel_max = 1e3
+)
+# We will only cosider a subset of all the model parameters
+wells = well_symbols(proxy_cal.model)
+for (k, v) in opt_config
+    for (ki, vi) in v
+        if ki in [ # Volumetric properties
+            :FluidVolume, :BulkVolume
+            ]
+            vi[:active] = k == :Reservoir
+            vi[:rel_min] = 1e-3
+            vi[:rel_max] = 1e2
+        elseif ki in [ # Rock density and heat capacity
+            :RockDensity, :RockHeatCapacity
+            ]
+            vi[:active] = k == :Reservoir
+            vi[:rel_min] = 1e-3
+            vi[:rel_max] = 1e3
+        elseif ki in [ # Conductive properties
+            :Transmissibilities,
+            :RockThermalConductivities, :FluidThermalConductivities
+            ]
+            vi[:active] = k == :Reservoir
+            vi[:rel_min] = 1e-6
+            vi[:rel_max] = 1e3
+        elseif ki in [ # Well properties
+            :WellIndices, :WellIndicesThermal
+            ]
+            vi[:active] = k in wells
+            vi[:rel_min] = 1e-3
+            vi[:rel_max] = 1e2
+        else
+            vi[:active] = false
+        end
+    end
+end
+
+## ### Calibrate proxy model
+# Setting up the calibration requires a few steps, which has been conveniently
+# implemented in the `calibrate_case` utility function. We use the LBFGS
+# optimization algorithm, which has a number parameters that can be set,
+# including the maximum number of function evaluations (maxfun), and the maximum
+# number of iterations (maxiter), both we set to 200 here. Increasing these
+# numbers will likely give a better match.
+proxy_cal = calibrate_case(objective, proxy, n_steps, opt_config; 
+    lbfgs_args = (maxfun = 500, maxiter = 500))
+
+# ### Simulate the full schedule using the calibrated proxy
+results_proxy_cal = simulate_reservoir(proxy_cal)
+states_proxy_cal = results_proxy_cal.result.states
+obj = Jutul.evaluate_objective(
+    objective, proxy_cal.model, states_proxy_cal[1:n_steps], 
+    proxy_cal.dt[1:n_steps], proxy_cal.forces[1:n_steps])
+println("Final proxy mismatch: $obj")
+
+## ### Plot the calibrated results
+# Finally, we plot the resulting prduction temperatures for the high-fidelity
+# and proxy model. The calibrated proxy does a good job of reproducing the
+# temperatures used for calibration, but the prediction for the remaining three
+# years of storage are not perfect, with the calibrated proxy model being
+# slightly worse that the initial proxy model for WellA in the final year.
+time_tot = results_hifi.wells.time/si_unit(:year)
+for well in well_symbols(hifi.model)
+    fig = plot_well_data(time_tot, states_hf, 
+        vcat([states_proxy], [states_proxy_cal], [states_proxy_cal]);
+        wells = [well],
+        field = :Temperature,
+        nan_ix = [
+            missing, 
+            n_steps+1:length(hifi.dt),
+            1:n_steps-1],
+        names=vcat(
+            "High-fidelity", 
+            "Proxy (initial)", 
+            "Proxy (calibration)",
+            "Proxy (prediction)")
+        )
+    display(GLMakie.Screen(), fig)
+end
---
+++ src/cases/egg_geothermal.jl
@@ -0,0 +1,293 @@
+"""
+    egg_geothermal(; <keyword arguments>)
+
+# Keyword arguments
+
+- `include_wells = true`: Include wells in the model.
+- `wells_distance_ij = 20`: Distance (in cell numbers) between the wells in i
+  and j directions.
+- simple_well = false: Use simple well model instead of the full well model.
+- geothermal_gradient = 0.03si_unit(:Kelvin)/si_unit(:meter): Geothermal
+  gradient in the reservoir.
+- pressure_top = 50si_unit(:bar): Pressure at the top of the reservoir.
+- temperature_top = convert_to_si(50, :Celsius): Temperature at the top of the
+  reservoir.
+- use_bc = true: Use fixed Dirichlet pressure and temperature boundary
+  conditions at the reservioir sides.
+
+"""
+function egg_geothermal(;
+    include_wells = true,
+    well_distance_ij = 20,
+    simple_well = false,
+    geothermal_gradient = 0.03si_unit(:Kelvin)/si_unit(:meter),
+    pressure_top = 50si_unit(:bar),
+    temperature_top = convert_to_si(50, :Celsius),
+    use_bc = true,
+    )
+
+    # ## Load EGG data from file
+    egg_dir = JutulDarcy.GeoEnergyIO.test_input_file_path("EGG")
+    data_pth = joinpath(egg_dir, "EGG.DATA")
+    case0 = setup_case_from_data_file(data_pth)
+    domain = reservoir_model(case0.model).data_domain
+
+    # ## Set up wells
+    if include_wells
+        d_ij = ceil(well_distance_ij/2)
+        d_ij = clamp(d_ij,1,29)
+        inj_well = setup_vertical_well(domain, 30 + d_ij, 30 - d_ij;
+            name = :WellA, simple_well = simple_well)
+        prod_well = setup_vertical_well(domain, 30 - d_ij, 30 + d_ij;
+            name = :WellB, simple_well = simple_well)
+        obs_well = setup_vertical_well(domain, 30, 30;
+            name = :WellObs, simple_well = simple_well)
+        wells = [inj_well, prod_well, obs_well]
+    else
+        wells = []
+    end
+
+    # ## Set up reservoir model
+    model, parameters = setup_reservoir_model(
+        domain, :geothermal,
+        thermal = true,
+        wells = wells
+    );
+    rmodel = reservoir_model(model)
+    push!(rmodel.output_variables, :PhaseMassDensities, :PhaseViscosities)
+
+    # ## Get geometry and gradients
+    mesh = physical_representation(rmodel.data_domain)
+    geo = tpfv_geometry(mesh)
+    z = geo.cell_centroids[3,:]
+    depth = z .- minimum(z)
+    rho_ref = rmodel.system.rho_ref[1]
+    dp_dz = rho_ref.*9.81
+    dT_dz = geothermal_gradient
+
+    # ## Set up initial state
+    state0 = setup_reservoir_state(model,
+        Pressure = pressure_top .+ dp_dz.*depth,
+        Temperature = temperature_top .+ dT_dz.*depth
+    )
+
+    # ## Set up forces
+    if use_bc
+        sides = abs.(geo.boundary_normals[3,:]) .== 0
+        cells = mesh.boundary_faces.neighbors[sides]
+        bc_cells, bc_pressure, bc_temperature = [], [], []
+        for c in cells
+            push!(bc_cells, c)
+            push!(bc_pressure, pressure_top + dp_dz*depth[c])
+            push!(bc_temperature, temperature_top + dT_dz*depth[c])
+        end
+        bc = flow_boundary_condition(bc_cells, domain, bc_pressure, bc_temperature)
+    else
+        bc = nothing
+    end
+
+    dt = 100si_unit(:year)
+    forces = setup_reservoir_forces(model, bc = bc)
+    
+    # ## Return case
+    return JutulCase(model, [dt], [forces], state0 = state0, parameters = parameters)
+
+end
+
+"""
+    egg_geothermal_doublet(; <keyword arguments>)
+
+# Keyword arguments
+- `rate_injection = 10si_unit(:litre)/si_unit(:second)`: Injection rate.
+- `rate_production = rate_injection`: Production rate.
+- `temperature_injection = convert_to_si(10.0, :Celsius)`: Injection temperature.
+- `rate_observation = missing`: Observation well rate. Set to
+  min(rate_injection, rate_production)/1000 if missing.
+- `bhp = 45*si_unit(:bar)`: Bottom hole pressure of supporting well if BCs are
+  not given (default - set use_bc = true to force BCs).
+- `report_interval = si_unit(:year)/4`: Report interval for simulation results.
+- `production_time = 25.0si_unit(:year)`: Production time.
+- `well_distance_ij = 30`: Distance between the wells in i and j directions.
+- All other keyword arguments are passed to `egg_geothermal`.
+
+""" 
+function egg_geothermal_doublet(;
+    rate_injection = 10si_unit(:litre)/si_unit(:second),
+    rate_production = rate_injection,
+    temperature_injection = convert_to_si(10.0, :Celsius),
+    rate_observation = missing,
+    bhp = 45*si_unit(:bar),
+    report_interval = si_unit(:year)/4,
+    production_time = 25.0si_unit(:year),
+    kwargs...
+    )
+
+    # ## Set up base case
+    case0 = egg_geothermal(;
+        well_distance_ij = 30,
+        temperature_top = convert_to_si(90.0, :Celsius),
+        use_bc = false,
+        kwargs...
+        )
+
+    # ## Set up forces
+    rmodel = reservoir_model(case0.model)
+    rho_ref = rmodel.system.rho_ref[1]
+    bc = case0.forces[:Reservoir].bc
+    # Set observation well control
+    if ismissing(rate_observation)
+        rate_observation = 1e-3*min(rate_injection, rate_production)
+    end
+    target = TotalRateTarget(-rate_observation)
+    ctrl_obs = ProducerControl(target)
+    # Set controls
+    if isnothing(bc)
+        target = BottomHolePressureTarget(bhp)
+    else 
+        target = TotalRateTarget(-rate_production)
+    end
+    ctrl_prod = ProducerControl(target)
+    target = TotalRateTarget(rate_injection)
+    ctrl_inj = InjectorControl(target, [1.0];
+        density = rho_ref, temperature = temperature_injection)
+    control = Dict(:WellA => ctrl_prod, :WellB => ctrl_inj, :WellObs => ctrl_obs)
+    # Define reservoir forces
+    forces = setup_reservoir_forces(case0.model, control = control, bc = bc)
+    # Set report steps
+    n_step = Int(floor(production_time/report_interval))
+    dt = fill(report_interval, n_step)
+    time_residual = production_time - sum(dt)
+    if time_residual < 0.1*report_interval
+        dt[end] += time_residual
+    else
+        dt = vcat(dt, time_residual)
+    end
+    forces = fill(forces, n_step)
+
+    # ## Return case
+    case = JutulCase(case0.model, dt, forces, 
+        state0 = case0.state0, parameters = case0.parameters)
+    return case
+
+end
+
+
+"""
+    egg_ates(;
+    temperature_chare = convert_to_si(90.0, :Celsius),
+    temperature_dischare = convert_to_si(10.0, :Celsius),
+    rate_charge = 25si_unit(:litre)/si_unit(:second),
+    rate_discharge = rate_charge,
+    rate_observation = missing,
+    bhp_charge = 25.0si_unit(:bar),
+    bhp_discharge = 45.0si_unit(:bar),
+    charge_months = ("June", "July", "August", "September"),
+    discharge_months = ("December", "January", "February", "March"),
+    report_interval = si_unit(:year)/12,
+    num_years = 5,
+    kwargs...
+    )
+
+# Keyword arguments
+- `temperature_chare = convert_to_si(90.0, :Celsius)`: Charge temperature.
+- `temperature_dischare = convert_to_si(10.0, :Celsius)`: Discharge temperature.
+- `rate_charge = 25si_unit(:litre)/si_unit(:second)`: Charge rate.
+- `rate_discharge = rate_charge`: Discharge rate.
+- `rate_observation = missing`: Observation well rate. Set to
+  min(rate_charge, rate_discharge)/1000 if missing.
+- `bhp_charge = 25.0si_unit(:bar)`: Bottom hole pressure of supporting well
+  during charge if BCs are not given (not default - set use_bc = false to force
+  no BCs).
+- `bhp_discharge = 45.0si_unit(:bar)`: Bottom hole pressure of supporting well
+  during discharge if BCs are not given (not default - set use_bc = false to
+  force no BCs).
+- `charge_months = ("June", "July", "August", "September")`: Months for charge.
+- `discharge_months = ("December", "January", "February", "March")`: Months for
+  discharge.
+- `num_years = 5`: Number of years to simulate.
+- `report_interval = si_unit(:year)/12`: Report interval for simulation results.
+- All other keyword arguments are passed to `egg_geothermal`.
+
+"""
+function egg_ates(;
+    temperature_chare = convert_to_si(90.0, :Celsius),
+    temperature_dischare = convert_to_si(10.0, :Celsius),
+    rate_charge = 25si_unit(:litre)/si_unit(:second),
+    rate_discharge = rate_charge,
+    rate_observation = missing,
+    bhp_charge = 25.0si_unit(:bar),
+    bhp_discharge = 45.0si_unit(:bar),
+    charge_months = ("June", "July", "August", "September"),
+    discharge_months = ("December", "January", "February", "March"),
+    num_years = 5,
+    report_interval = si_unit(:year)/12,
+    kwargs...
+    )
+
+    # ## Get base case
+    case0 = egg_geothermal(; kwargs...)
+
+    # ## Set up forces
+    rmodel = reservoir_model(case0.model)
+    rho_ref = rmodel.system.rho_ref[1]
+    bc = case0.forces[1][:Reservoir].bc
+    # Set observation well control
+    if ismissing(rate_observation)
+        rate_observation = 1e-3*min(rate_charge, rate_discharge)
+    end
+    target = TotalRateTarget(-rate_observation)
+    ctrl_obs = ProducerControl(target)
+    # Set charge controls
+    target = TotalRateTarget(rate_charge)
+    ctrl_hot = InjectorControl(target, [1.0];
+        density = rho_ref, temperature = temperature_chare)
+    if isnothing(bc)
+        target = BottomHolePressureTarget(bhp_charge)
+    else 
+        target = TotalRateTarget(-rate_charge)
+    end
+    ctrl_cold = ProducerControl(target)
+    control = Dict(:WellA => ctrl_hot, :WellB => ctrl_cold, :WellObs => ctrl_obs)
+    forces_charge = setup_reservoir_forces(case0.model, control = control, bc = bc)
+    # Set discharge controls
+    target = TotalRateTarget(-rate_discharge)
+    ctrl_hot = ProducerControl(target)
+    if isnothing(bc)
+        target = BottomHolePressureTarget(bhp_discharge)
+    else
+        target = TotalRateTarget(rate_discharge)
+    end
+    ctrl_cold = InjectorControl(target, [1.0];
+        density = rho_ref, temperature = temperature_dischare)
+    control = Dict(:WellA => ctrl_hot, :WellB => ctrl_cold, :WellObs => ctrl_obs)
+    forces_discharge = setup_reservoir_forces(case0.model, control = control, bc = bc)
+    # Set rest forces
+    forces_rest = setup_reservoir_forces(case0.model, bc = bc)
+    # Set up timesteps and assign forces to each timestep
+    dt_vec = Float64[]
+    forces = []
+    month = si_unit(:year)/12
+    n_step = Int(round(month/report_interval))
+    dt = month/n_step
+    for year in 1:num_years
+        for mno in vcat(6:12, 1:5)
+            mname = monthname(mno)
+            if mname in discharge_months
+                push!(dt_vec, fill(dt, n_step)...)
+                push!(forces, fill(forces_discharge, n_step)...)
+            elseif mname in charge_months
+                push!(dt_vec, fill(dt, n_step)...)
+                push!(forces, fill(forces_charge, n_step)...)
+            else
+                push!(dt_vec, fill(dt, n_step)...)
+                push!(forces, fill(forces_rest, n_step)...)
+            end
+        end
+    end
+
+    # ## Return case
+    case = JutulCase(case0.model, dt_vec, forces, 
+        state0 = case0.state0, parameters = case0.parameters)
+    return case
+
+end
