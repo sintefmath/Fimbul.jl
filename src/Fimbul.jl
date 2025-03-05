48183b549fdb9dd85266e7e007693d1cfa98a1dc
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2025-03-05 14:52:38 +0100

    Add analytical example

--- src/Fimbul.jl
+++ src/Fimbul.jl
@@ -3,8 +3,10 @@ module Fimbul
     using Jutul, JutulDarcy
     using Dates
     include("cases/egg_geothermal.jl")
+    include("cases/analytical.jl")
 
     export egg_geothermal, egg_geothermal_doublet, egg_ates
+    export beam_thermal
 
     using LBFGSB
     include("optimization/objectives.jl")
