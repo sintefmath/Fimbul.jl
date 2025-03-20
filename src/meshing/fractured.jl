3892455a6d7adc07423b3cb8753d4df987c9ed1c
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2025-03-20 14:56:18 +0100

    Fractured layers start and ends with matrix instead of layer

--- src/meshing/fractured.jl
+++ src/meshing/fractured.jl
@@ -15,23 +15,23 @@ function horizontal_fractured_mesh(cell_constraints, depths, num_fractures;
     z = [depths[1]]
     hz_all = []
     layer_map = Int64[]
-    fracture_map = []
+    fracture_map = Bool[]
     for i = 1:length(depths)-1
         dz = depths[i+1] - depths[i]
         if num_fractures[i] > 0
             @assert num_fractures[i] > 1
             nf = num_fractures[i]
-            dz_mat = (dz - nf*aperture)/(nf-1)
-            dz = repeat([aperture, dz_mat], nf)[1:end-1]
+            dz_mat = (dz - nf*aperture)/(nf+1)
+            dz = repeat([dz_mat, aperture], nf+1)[1:end-1]
             zi = cumsum(dz) .+ depths[i]
             if ismissing(hz)
                 hzi = dz_mat/5
             else
                 hzi = hz[i]
             end
-            hzi = fill(hzi, 2*nf-1)
+            hzi = fill(hzi, 2*nf+1)
             @assert isapprox(zi[end], depths[i+1])
-            fmap = repeat([true, false], nf)[1:end-1]
+            fmap = repeat([false, true], nf+1)[1:end-1]
         else
             zi = depths[i+1]
             if ismissing(hz)
@@ -50,7 +50,7 @@ function horizontal_fractured_mesh(cell_constraints, depths, num_fractures;
     end
 
     mesh, layers, metrics = extruded_mesh(cell_constraints, z; hz = hz_all, kwargs...)
-    
+
     fractures = fracture_map[layers]
     layers = layer_map[layers]
     
