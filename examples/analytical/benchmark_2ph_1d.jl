de5b392f4b102d8f4e3b3743c13852e90d0611c7
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2026-05-11 15:32:52 +0200

    Update

--- examples/analytical/benchmark_2ph_1d.jl
+++ examples/analytical/benchmark_2ph_1d.jl
@@ -6,7 +6,7 @@ using NetworkLayout, LayeredLayouts, GraphMakie
 tabs = Fimbul.build_steam_tables_2ph()
 
 ## Case a, no gravity (default)
-case_a = benchmark_2ph_1d(benchmark_case = :a, nx = 100, enthalpy_tables = tabs, gravity = false)
+case_a = benchmark_2ph_1d(benchmark_case = :b, nx = 100, enthalpy_tables = tabs, gravity = false)
 
 ##
 st = deepcopy(case_a.state0[:Reservoir])
@@ -19,38 +19,45 @@ st = Jutul.evaluate_all_secondary_variables(case_a.model.models[:Reservoir], st)
 sim, cfg = setup_reservoir_simulator(case_a;
     tol_mb = 1e-5,
     info_level = 2,
+    max_timestep = maximum(case_a.dt)*0.1,
+    max_nonlinear_iterations = 1000,
 );
 
-result = simulate_reservoir(case_a, simulator=sim, config=cfg)
+n_step = 48
+result = simulate_reservoir(case_a[1:min(n_step, length(case_a.dt))], simulator=sim, config=cfg)
 
 ##
 plot_reservoir(case_a.model, result.states)
 
 ##
 using GLMakie
+using GLMakie
 GLMakie.closeall()
 timestep = length(result.states)÷2
 time = 120si"year"
 timestep = findfirst(t -> t >= time, result.time)
 timestep = isnothing(timestep) ? length(result.states) : timestep
+timestep = length(result.states)
 
 x = tpfv_geometry(physical_representation(case_a.model.models[:Reservoir].data_domain)).cell_centroids[1,:]
 z = tpfv_geometry(physical_representation(case_a.model.models[:Reservoir].data_domain)).cell_centroids[3,:]
-
 # x = z
 
 fig = Figure()
-for (k, prop) in enumerate([:Temperature, :Pressure, :Saturations, :TotalThermalEnergy, :PhaseMassDensities])
+for (k, prop) in enumerate([:Temperature, :Pressure, :Saturations, :PhaseMassDensities, :WaterPhase, :PhaseViscosities])
     ax = Axis(fig[1, k], title = string(prop))
     y = result.states[timestep][prop]
-    if prop == :Saturations
-        y = vec(y[1,:])
-    elseif prop == :Temperature
-        y = convert_from_si.(y, :Celsius)
-    elseif prop == :Pressure
-        y = convert_from_si.(y, "megapascal")
+    if size(y, 1) == 2
+        for ph in 1:2
+            lines!(ax, x, vec(y[ph,:]), label = "Phase $ph")
+        end
+    else
+        if prop == :Temperature
+            y = convert_from_si.(y, :Celsius)
+        elseif prop == :Pressure
+            y = convert_from_si.(y, "megapascal")
+        end
+        lines!(ax, x, y)
     end
-    lines!(ax, x, y)
 end
 display(fig)
-
