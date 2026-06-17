425db5791c9fffddea1dc54cd33adb537b8562db
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2026-06-17 22:30:40 +0200

    Update name

--- src/Fimbul.jl
+++ src/Fimbul.jl
@@ -30,7 +30,7 @@ module Fimbul
     export thermal_radius_aquifer
     # Two-phase geothermal
     export H2OSystem
-    export build_steam_tables_2ph
+    export build_steam_tables_h2o
 
     # Load dependencies into namespace
     using Jutul, JutulDarcy
